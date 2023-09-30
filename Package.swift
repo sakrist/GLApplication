2c4ccbcc58bbb16d9ee54ad119d03d4b75d11135
Volodymyr B <110199+sakrist@users.noreply.github.com>
2023-10-01 00:04:53 +0100

    fix dependencies

--- Package.swift
+++ Package.swift
@@ -14,15 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SwiftGFX/SwiftMath", from: "3.3.1"),
-        .package(url: "https://github.com/compnerd/swift-win32", branch: "main")
+        .package(url: "https://github.com/SwiftGFX/SwiftMath", from: "3.3.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GLApplication",
-            dependencies: [ "SwiftMath", .product(name: "SwiftWin32", package: "swift-win32") ],
+            dependencies: [ "SwiftMath" ],
             cSettings: [.define("GL_GLEXT_PROTOTYPES")]),
         .testTarget(
             name: "GLApplicationTests",
@@ -54,7 +53,7 @@ package.targets[0].dependencies += [ "OpenGL", "X11" ]
 package.targets += [
     .systemLibrary(name: "WinGL"),
 ]
-
-package.targets[0].dependencies += [ "WinGL" ]
+package.dependencies += [ .package(url: "https://github.com/compnerd/swift-win32", branch: "main") ]
+package.targets[0].dependencies += [ "WinGL", .product(name: "SwiftWin32", package: "swift-win32") ]
 
 #endif
